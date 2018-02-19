137ab0836b5fd1f66800023bfae4eb1576bed919
GwonHyeok <me@ghyeok.io>
2018-02-19 20:02:56 +0900

    me 회사 정보 추가

--- routes/me.js
+++ routes/me.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const Company = require('../models/company');
 
 router.get('/', passport.authenticate('bearer'), async (req, res) => {
   if (req.isAuthenticated()) {
+    const company = await Company.findById(req.user.company);
     return res.status(200).json({
       data: {
         name: req.user.name,
-        username: req.user.username
-
+        username: req.user.username,
+        company: company
       }
     });
   }
