70605f5978db4bad645af269df8e7e442231a64b
GwonHyeok <me@ghyeok.io>
2018-02-19 20:30:32 +0900

    me 키워드 로그 api 추가

--- routes/me.js
+++ routes/me.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const Company = require('../models/company');
+const Keyword = require('../models/keyword');
+const KeywordLog = require('../models/keywordLog');
 
 router.get('/', passport.authenticate('bearer'), async (req, res) => {
   if (req.isAuthenticated()) {
@@ -16,6 +18,23 @@ router.get('/', passport.authenticate('bearer'), async (req, res) => {
   }
 
   res.status(401).json({ error: 'is not authorized' })
+});
+
+router.get('/keywordLogs', passport.authenticate('bearer'), async (req, res) => {
+  if (!req.isAuthenticated()) return res.status(401).json();
+
+  const company = await Company.findById(req.user.company);
+  const keywordLogs = await KeywordLog.find({
+    $and: [
+      {
+        $or: company.keywords.map(keyword => {
+          return { keyword: keyword }
+        })
+      },
+    ]
+  }).populate('keyword');
+
+  res.json({ meta: { count: keywordLogs.length }, data: keywordLogs });
 });
 
 module.exports = router;
