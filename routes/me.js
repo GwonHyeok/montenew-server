4245f69209537dfc1b0481a4ee74ed877927f865
GwonHyeok <me@ghyeok.io>
2018-02-19 19:27:23 +0900

    me 검증 api 추가

--- routes/me.js
+++ routes/me.js
@@ -2,8 +2,18 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-router.get('/', function(req, res) {
-  res.json({ data: req.user })
+router.get('/', passport.authenticate('bearer'), async (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.status(200).json({
+      data: {
+        name: req.user.name,
+        username: req.user.username
+
+      }
+    });
+  }
+
+  res.status(401).json({ error: 'is not authorized' })
 });
 
 module.exports = router;
